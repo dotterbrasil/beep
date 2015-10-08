b4ed08f96a18d71ac00dfda10a6783b6bf25d95c
dotterbrasil <eduardo@dotter.com.br>
2015-10-08 16:26:01 -0300

    AJustes

--- js/funcoes.js
+++ js/funcoes.js
@@ -31,34 +31,18 @@ if (navigator.geolocation) {
 	
 }
 
-function showPosition_old(position) {
-    document.getElementById("mapa").innerHTML = "Latitude: " + position.coords.latitude + 
-    "<br>Longitude: " + position.coords.longitude;	
-	
-	initMap()
-}
 
-function initMap() {
-  // Create a map object and specify the DOM element for display.
-  var map = new google.maps.Map(document.getElementById('mapa'), {
-    center: {lat: -34.397, lng: 150.644},
-    scrollwheel: false,
-    zoom: 8
-  });
-}
 
  function showPosition(position) {
     var latlon = position.coords.latitude + "," + position.coords.longitude;
 	
 	 document.getElementById("status").innerHTML = "Buscando localizacao no mapa...";
-    	
+    	PlaySound("sound2");
 	document.getElementById("mapa").style.backgroundImage = "url('imagens/fundo_alerta.gif')";
 	
      document.getElementById("mapa").innerHTML = "<iframe width=450px height=250px src='https://www.google.com/maps/embed/v1/place?q="+latlon+"&key=AIzaSyAj6LuyubKgTA8wlfqsTzQHKkSlTO9ZMOc' allowfullscreen align='center'></iframe><br><img src='imagens/alert.gif' class='alerta' onclick='home();'>";
 	 
 	 document.getElementById("status").innerHTML = "Clique sobre o ALERT para desativar o alarme!";
-	 
-	 PlaySound("sound2");
 	  
  }
  
@@ -82,9 +66,9 @@ function initMap() {
   var url = getMediaURL("sons/alerta.wav");
   
   var my_media = new Media(url,null,mediaError);
-  while(1<3){
+  
   my_media.play();
-  }
+  
 }
 
 function getMediaURL(s) {
