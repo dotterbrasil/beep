2f7e9b54875fd4e7fcba76b6cce34b9e3b540c2e
dotterbrasil <eduardo@dotter.com.br>
2015-10-16 10:40:36 -0300

    Auto ajuste das margens

--- js/funcoes.js
+++ js/funcoes.js
@@ -251,7 +251,10 @@ var itens = "";
 for (var i=0; i<indice; ++i)
 	{
 	localStorage.setItem("in"+i,localStorage.getItem("kid"+i));
-	itens = itens + "<div class='lista'>"+localStorage.getItem("in"+i) + " <a href='javascript:check_out("+i+");' class='green'>CHECK OUT</a></div>";
+	itens = itens + "<div id='lista' class='lista'>"+localStorage.getItem("in"+i) + " <a href='javascript:check_out("+i+");' class='green'>CHECK OUT</a></div>";
+	
+	document.getElementById("mapa").style.margin-top = 80/(2*indice)+"%";
+	document.getElementById("mapa").style.margin-bottom = 80/(2*indice)+"%";
 	}
 
 if(indice>0)
