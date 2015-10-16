9ec50758039c8c4e653d67f2032ec6a986fa3784
dotterbrasil <eduardo@dotter.com.br>
2015-10-16 13:21:20 -0300

    ao quadrado

--- js/funcoes.js
+++ js/funcoes.js
@@ -177,7 +177,7 @@ if (typeof(Storage) !== "undefined")
 			document.getElementById("principal").innerHTML = "<br><br><font face='sans-serif'>" + itens + "</font>";
 			document.getElementById("principal").style.backgroundImage = "url('imagens/fundo.gif')";
 			
-			proporcao = (35 - indice*20).toString()+"%";
+			proporcao = (35 - indice*indice).toString()+"%";
 			document.getElementById("principal").style.paddingTop = proporcao;
 			document.getElementById("principal").style.paddingBottom = proporcao;
 			}
