7318b64f9120cda59048bf7ff07f824b95166e53
dotterbrasil <eduardo@dotter.com.br>
2015-12-02 16:14:11 -0200

    iswalking

--- js/funcoes.js
+++ js/funcoes.js
@@ -161,8 +161,11 @@ function onSuccess(position) {
 	
 	if (position.coords.speed < 2)
 		{
-		notificacao_local('VELOCIDADE','Avaliando saida do carro.', 2);
-		temporizador = setTimeout(onError, 30000); //faz checkout forcado apos 5 minutos de baixa velocidade
+		if (plugado == "false")
+			{
+			notificacao_local('VELOCIDADE','Avaliando saida do carro.', 2);
+			iswalking();
+			}
 		}
 		else{
 			if (position.coords.speed > 5) {clearTimeout(temporizador);} //cancela checkout forcado se velocidade subir
@@ -768,4 +771,102 @@ function onBatteryStatus(info) {
 				if (plugado == true) { document.location.href = "index.html"; }
 				}
 			
+	}
+	
+	
+//---------------------------------------------------------------------------- Avaliacao de Caminhada  ----------------------------------------------------------------------------
+
+	var loop = 0;	
+	var contador = 0;
+	var soma_eixo = 0;
+	var media = 0;
+	var array_eixo = new Array();
+	var anterior = false;
+	
+	
+	function iswalking(){ 
+	//	document.getElementById("status").innerHTML = "";
+		
+		loop = 0;	
+		contador = 0;
+		soma_eixo = 0;
+		media = 0;
+		array_eixo = new Array();
+		anterior = false;
+		
+		accelerometer();
+	}		
+	function accelerometer(){ 
+		navigator.accelerometer.getCurrentAcceleration(motion, error);
+	}
+	function error(){
+		alert('Error!');
+	}
+	function motion(acceleration){
+		var x = acceleration.x;
+		var x_int = Math.round(x);
+		var accel_x = Math.pow(x_int, 2);
+		
+		var y = acceleration.y;
+		var y_int = Math.round(y);
+		var accel_y = Math.pow(y_int, 2);
+		
+		var z = acceleration.z;
+		var z_int = Math.round(z);
+		var accel_z = Math.pow(z_int, 2);
+		
+		if(loop < 50){			
+		//	document.getElementById("status").innerHTML = "<br><h2>"+loop+"</h2>"; 		
+	
+			var xyz = accel_x + accel_y + accel_z;
+			var xyzSqrt = Math.sqrt(xyz);
+			var eixo = Math.round(xyzSqrt);
+			array_eixo.push(eixo);
+			
+			soma_eixo = soma_eixo + eixo;
+			loop++;
+			setTimeout(accelerometer, 100);	
+		}
+		else
+		{		
+			var m = soma_eixo / 50;
+			media = Math.round(m);
+			
+			for (i = 0; i < 50; i++){	
+				var v = array_eixo[i] - media;
+				var vetor = Math.round(v);
+				
+				if(!anterior){
+					if(vetor > 1){
+						contador++;
+						anterior = true;					
+					}
+					else if (vetor <= '-2')
+					{
+						anterior = false;
+					}
+				}
+				else 
+				{
+					if(vetor <= '-2') {
+						anterior = false; 
+					}
+				}  
+			} 
+			frequencia = contador / 5;
+			
+		//	document.getElementById("status").innerHTML += "<br><h2>Result</h2><br>Frequência "+ frequencia; 	
+		
+			if(frequencia > '1.2' && frequencia < '3.4'){
+			//	document.getElementById("status").innerHTML += "<br><h2>Is Walking</h2>"; 	
+				notificacao_local('VELOCIDADE','Motorista esta andando a pe.', 2);
+				onError();
+				return true;
+			}
+			else
+			{
+			//	document.getElementById("status").innerHTML += "<br><h2>Is not Walking</h2>"; 
+				return false;
+			}
+		}
 	}
