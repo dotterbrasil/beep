b5ca132250d152beaea40c75fccb2e97a833b288
dotterbrasil <eduardo@dotter.com.br>
2015-12-08 14:40:02 -0200

    Velocidade correta

--- js/funcoes.js
+++ js/funcoes.js
@@ -144,7 +144,7 @@ function onSuccessIN(position) {
 	
 	//window.plugins.backgroundjs.lockBackgroundTime();
 	
-	if(position.coords.speed > 1)
+	if(position.coords.speed > 5)
 		{
 		if (plugado == "true")
 			{
@@ -178,7 +178,7 @@ function onSuccess(position) {
 	
 	//window.plugins.backgroundjs.lockBackgroundTime();
 	
-	if (position.coords.speed < 1)
+	if (position.coords.speed < 2)
 		{
 		if (plugado == "false")
 			{
