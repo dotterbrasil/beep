0d56226bb454f016be3a97e1f3f35909d71988df
dotterbrasil <eduardo@dotter.com.br>
2016-01-14 14:25:39 -0200

    ..

--- js/funcoes.js
+++ js/funcoes.js
@@ -197,13 +197,13 @@ function XSuccess(acceleration) {
 	   
 	   //if (Math.sqrt(Math.pow(acceleration.x - pre_accX,2))<1)	{	accX = 0;	}
 	//		else {		accX = acceleration.x;		}
-	   accX = (acceleration.x - pre_accX);
+	   accX = (acceleration.x - pre_accX) / 2;
 	 //  if (Math.sqrt(Math.pow(acceleration.y - pre_accY,2))<1)	{	accY = 0;	}
 	//		else {		accY = acceleration.y;		}
-			accY = (acceleration.y - pre_accY);
+			accY = (acceleration.y - pre_accY) / 2;
 	//	if (Math.sqrt(Math.pow(acceleration.z - pre_accZ,2))<1)	{	accZ = 0;	}
 	//		else {		accZ = acceleration.z;		}
-	  	   accZ = (acceleration.z - pre_accZ);
+	  	   accZ = (acceleration.z - pre_accZ) / 2;
 		   
 	   pre_accX = acceleration.x;
 	   pre_accY = acceleration.y;
@@ -213,8 +213,8 @@ function XSuccess(acceleration) {
 	   yspeed = yspeed + accY;
 	   zspeed = zspeed + accZ;
 	   
-	   xyzspeed = Math.round(Math.sqrt(Math.sqrt(Math.pow(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 100, 2))) * 3.6);
-	   //xyzspeed = Math.round(Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2)) * 3.6);
+	   //xyzspeed = Math.round(Math.sqrt(Math.sqrt(Math.pow(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 97, 2))) * 3.6);
+	   xyzspeed = Math.round(Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2)) * 3.6);
 	   //notificacao_local('Velocidade X',xspeed, 1);
 	   
 	    
