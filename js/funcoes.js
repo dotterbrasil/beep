5c40b366aa9dfd21e8f64a259e52b1ef96806e3c
dotterbrasil <eduardo@dotter.com.br>
2016-01-14 14:31:35 -0200

    Agora sim

--- js/funcoes.js
+++ js/funcoes.js
@@ -214,7 +214,7 @@ function XSuccess(acceleration) {
 	   zspeed = zspeed + accZ;
 	   
 	   //xyzspeed = Math.round(Math.sqrt(Math.sqrt(Math.pow(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 97, 2))) * 3.6);
-	   xyzspeed = Math.round(Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2)) * 3.6);
+	   xyzspeed = Math.round(Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 24.157225) * 3.6);
 	   //notificacao_local('Velocidade X',xspeed, 1);
 	   
 	    
