88981a7aa985f8decf5b077659ebbf80bb7eb0fe
dotterbrasil <eduardo@dotter.com.br>
2016-01-14 10:42:12 -0200

    hehe

--- js/funcoes.js
+++ js/funcoes.js
@@ -218,7 +218,8 @@ function XSuccess(acceleration) {
 	   yspeed = yspeed + accY * delta_tempo;
 	   zspeed = zspeed + accZ * delta_tempo;
 	   
-	   xyzspeed = Math.round(Math.sqrt(Math.sqrt(Math.pow(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 96.6289, 2))) * 3.6);
+	   //xyzspeed = Math.round(Math.sqrt(Math.sqrt(Math.pow(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2) - 96.6289, 2))) * 3.6);
+	   xyzspeed = Math.round(Math.sqrt(Math.pow(xspeed, 2) + Math.pow(yspeed, 2) + Math.pow(zspeed, 2)) * 3.6);
 	   //notificacao_local('Velocidade X',xspeed, 1);
 	   
 	    
