63a43073f5860e5444eefc87983b2826a9de1e8a
dotterbrasil <eduardo@dotter.com.br>
2016-01-15 10:53:54 -0200

    .

--- js/funcoes.js
+++ js/funcoes.js
@@ -225,18 +225,19 @@ function XSuccess(acceleration) {
 			{componentes = componentes + "x";
 			accX = 0;
 			}
-			else {
-				if (modulo(acceleration.x)>2){accX = acceleration.x;}
-				if ((modulo(acceleration.y)>2)&&(modulo(acceleration.y - pre_accY)<2))
+			else { if (modulo(acceleration.x)>2){accX = acceleration.x;} }
+			}
+		if ((modulo(acceleration.y)>2)&&(modulo(acceleration.y - pre_accY)<2))
 					{componentes = componentes + "y";
 					accY = 0;
 					}
 					else {
 						if (modulo(acceleration.y)>2){accY = acceleration.y;}
-						if ((modulo(acceleration.z)>2)&&(modulo(acceleration.z - pre_accZ)<2)) {componentes = componentes + "z";accZ = 0; }
-							else {		if (modulo(acceleration.z)>2){accZ = acceleration.z;}	}
-						}
-				}
+					}
+		if ((modulo(acceleration.z)>2)&&(modulo(acceleration.z - pre_accZ)<2)) {componentes = componentes + "z";accZ = 0; }
+					else {		if (modulo(acceleration.z)>2){accZ = acceleration.z;}	}
+						
+				
 		
 		
 		   
