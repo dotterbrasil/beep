4e0185e966f199699e63df12483d70c7b93ac057
dotterbrasil <eduardo@dotter.com.br>
2016-01-18 14:33:40 -0200

    x

--- js/funcoes.js
+++ js/funcoes.js
@@ -233,12 +233,12 @@ if (leitura < 10)
 		pre_accY = pre_accY / 10;
 		pre_accZ = pre_accZ / 10;
 		accxyz = Math.sqrt( Math.pow(pre_accX ,2) + Math.pow(pre_accY, 2) + Math.pow(pre_accZ, 2) );
-		element.innerHTML = "X: " + pre_accX + "<br>Y: " + pre_accY + "<br>Z: " + pre_accZ + "<br>total: " + accxyz + "<br>pre: " + pre_accxyz + "<br>variacao: " + modulo((accxyz - pre_accxyz));
+		//element.innerHTML = "X: " + pre_accX + "<br>Y: " + pre_accY + "<br>Z: " + pre_accZ + "<br>total: " + accxyz + "<br>pre: " + pre_accxyz + "<br>variacao: " + ((accxyz - pre_accxyz));
 		if (leitura == 0) {pre_accxyz = accxyz;}
 		if ((accxyz/3)<3.3)
 			{accxyz = 0;} //elimina aceleracao quando unico componente for a gravidade
 			else{accxyz = Math.sqrt(modulo(Math.pow(pre_accX ,2) + Math.pow(pre_accY, 2) + Math.pow(pre_accZ, 2) - 95.90447) );}
-		//acctes(accxyz);
+		acctes((accxyz - pre_accxyz));
 		//element.innerHTML = "X: " + pre_accX + "<br>Y: " + pre_accX + "<br>Z: " + pre_accZ + "<br>total: " + accxyz;
 		
 		pre_accX = 0;
