a77bfdbac50d6122dc2c4a8f7e19bc96efbfbdab
dotterbrasil <eduardo@dotter.com.br>
2016-02-03 16:49:38 -0200

    ok mais uma

--- js/funcoes.js
+++ js/funcoes.js
@@ -67,9 +67,11 @@ function share(){
 	
 	var icone = "file://icon.png";
 	
-	window.plugins.socialsharing.share('Message, subject, image and link', 'The subject', 'https://www.google.nl/images/srpr/logo4w.png', 'http://www.dotter.com.br');
+	//window.plugins.socialsharing.share('Message, subject, image and link', 'The subject', 'https://www.google.nl/images/srpr/logo4w.png', 'http://www.dotter.com.br');
 	
-window.plugins.socialsharing.shareViaFacebook('Message via Facebook', 'www/icon.png' /* img */, 'http://www.dotter.com.br'/* url */,function() {alert('share ok');}, function(errormsg){alert(errormsg)});	
+//window.plugins.socialsharing.shareViaFacebook('Message via Facebook', icone, 'http://www.dotter.com.br',function() {alert('share ok');}, function(errormsg){alert(errormsg)});	
+
+window.plugins.socialsharing.shareViaFacebookWithPasteMessageHint('Message via Facebook', icone, 'http://www.dotter.com.br',function() {alert('share ok');}, function(errormsg){alert(errormsg)});	
 }
 
 function notificacao_local(tipo, mensagem_local, indice){
